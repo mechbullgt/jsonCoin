7966c36e62f7211cec4b30ad4499d65d9684b20a
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-07-21 23:30:53 +0530

    Linking schema.js to CLI get-schema

--- schema.js
+++ schema.js
@@ -1,15 +1,21 @@
+#!/usr/bin/env node
+//shebang character sequence to enable cli mode
+
 var schemaGenerator = require('generate-schema');
 const fs = require('fs');
+// For CLI
+const [,, ...args] = process.argv;
+console.log(`Hi ${args}`);
 
-fs.readFile('./responseJSON/Input.json',(err,data)=>{
-    if(err){
+fs.readFile('./responseJSON/Input.json', (err, data) => {
+    if (err) {
         console.log(err);
     } else {
         const jsonObject = JSON.parse(data);
         console.log(jsonObject);
         var schema = schemaGenerator.json(jsonObject);
         console.log(JSON.stringify(schema));
-        fs.writeFileSync('./resultJSONSchema/Output.json',JSON.stringify(schema));
+        fs.writeFileSync('./resultJSONSchema/Output.json', JSON.stringify(schema));
         console.log('JSON Schema generation successful!')
     }
 });
