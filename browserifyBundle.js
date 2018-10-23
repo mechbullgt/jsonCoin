54f95611e4e2b93357c45ae247c2fca4e8139e68
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-23 16:22:13 +0530

    Adding Browserify support.

--- browserifyBundle.js
+++ browserifyBundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 var fileSystem = require('fs');
 console.log("Front End Logic Js");
@@ -26,3 +27,6 @@ function fetchJSONFile(path, callback) {
 }
 
 window.fetchJSONFile = fetchJSONFile;
+},{"fs":2}],2:[function(require,module,exports){
+
+},{}]},{},[1]);
