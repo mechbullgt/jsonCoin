8e22d28f9ee900788547b779712f3f6c6d7d016e
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-23 17:48:04 +0530

    Major break through, client side functionality working.

--- assets/frontEndLogic.js
+++ assets/frontEndLogic.js
@@ -1,28 +1,39 @@
 "use strict";
-var fileSystem = require('fs');
+var fileSystem = require('generate-schema');
 console.log("Front End Logic Js");
 
-function readFileFromPath(path){
-    let rawData = fileSystem.readFileSync(path);
-    let theData = JSON.parse(rawData);
-    console.log("JSON Data:"+theData);
+function getJSONSchema(jsonInput){
+    console.log("In getJSONSchma() from frontEndLogic");
+    var inputJsonFile = JSON.parse(JSON.stringify(jsonInput));
+    console.log("Data from Client:"+inputJsonFile);
+    var theText = '{"Success":"true"}';
+    var outputJsonFile = JSON.parse(JSON.stringify(theText));
+    console.log("Output File:"+outputJsonFile);
+    return outputJsonFile;
 }
+window.getJSONSchema = getJSONSchema;
 
-window.readFileFromPath = readFileFromPath;
+// function readFileFromPath(path){
+//     let rawData = fileSystem.readFileSync(path);
+//     let theData = JSON.parse(rawData);
+//     console.log("JSON Data:"+theData);
+// }
 
-function fetchJSONFile(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-                console.log("Data:"+data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send(); 
-}
+// window.readFileFromPath = readFileFromPath;
 
-window.fetchJSONFile = fetchJSONFile;
+// function fetchJSONFile(path, callback) {
+//     var httpRequest = new XMLHttpRequest();
+//     httpRequest.onreadystatechange = function() {
+//         if (httpRequest.readyState === 4) {
+//             if (httpRequest.status === 200) {
+//                 var data = JSON.parse(httpRequest.responseText);
+//                 if (callback) callback(data);
+//                 console.log("Data:"+data);
+//             }
+//         }
+//     };
+//     httpRequest.open('GET', path);
+//     httpRequest.send(); 
+// }
+
+// window.fetchJSONFile = fetchJSONFile;
