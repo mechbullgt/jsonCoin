1affaeefccf538ca96336e908813db466f2c9988
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-24 00:22:39 +0530

    Code refactoring

--- browserifyBundle.js
+++ browserifyBundle.js
@@ -17,31 +17,6 @@ function getJSONSchema(jsonInput){
     return stringifySchema;
 }
 window.getJSONSchema = getJSONSchema;
-
-// function readFileFromPath(path){
-//     let rawData = fileSystem.readFileSync(path);
-//     let theData = JSON.parse(rawData);
-//     console.log("JSON Data:"+theData);
-// }
-
-// window.readFileFromPath = readFileFromPath;
-
-// function fetchJSONFile(path, callback) {
-//     var httpRequest = new XMLHttpRequest();
-//     httpRequest.onreadystatechange = function() {
-//         if (httpRequest.readyState === 4) {
-//             if (httpRequest.status === 200) {
-//                 var data = JSON.parse(httpRequest.responseText);
-//                 if (callback) callback(data);
-//                 console.log("Data:"+data);
-//             }
-//         }
-//     };
-//     httpRequest.open('GET', path);
-//     httpRequest.send(); 
-// }
-
-// window.fetchJSONFile = fetchJSONFile;
 },{"generate-schema":2}],2:[function(require,module,exports){
 exports.generic = require('./schemas/generic')
 exports.mongoose = require('./schemas/mongoose')
