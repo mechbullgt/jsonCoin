d194d6e318815dca25288cebc62c6e663713ba88
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-24 21:55:35 +0530

    Code refactored, removed webpack and added browserify.

--- logics/jsonSchemaLogic.js
+++ logics/jsonSchemaLogic.js
@@ -6,10 +6,6 @@ function getJSONSchema(jsonInput){
     console.log("In getJSONSchma() from frontEndLogic");
     var inputJsonFile = JSON.parse(JSON.stringify(jsonInput));
     console.log("Data from Client:"+inputJsonFile);
-    // var theText = '{"Success":"true"}';
-    // var outputJsonFile = JSON.parse(JSON.stringify(theText));
-    // console.log("Output File:"+outputJsonFile);
-
     var outSchemaFile = schemaGenerator.json(JSON.parse(jsonInput));
     var stringifySchema = JSON.stringify(outSchemaFile, null, 2);
     console.log("Schema:"+stringifySchema);
