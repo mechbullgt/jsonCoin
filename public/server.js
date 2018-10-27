820894424604b7b415b1220c1269a89360e530f3
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-27 22:43:13 +0530

    App works on local server, minor changes in express routing.

--- public/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const port =process.env.PORT || 8080;
-const app = express();
-var path = require('path');
-
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.listen(port);
