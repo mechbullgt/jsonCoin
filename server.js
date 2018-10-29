030ccb5f27b28057660f160367f1295f8fce5115
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-10-29 13:20:27 +0530

    Updated Title and added favicon.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
-// app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public'))
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'public/'));
